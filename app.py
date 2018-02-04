e7bf3aaea9b017486545e2290fb4c524f32783ff
김재균 <lonbekim@gmail.com>
2018-02-04 09:24:19 +0900

    mixpanel, add LoggingConsumer

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 from mixpanel import Mixpanel
 
+class LoggingConsumer(object):
+    def __init__(self):
+        self.mp_log = open("MIXPANEL_LOG.txt", "w+")
+
+    def send(self, endpoint, json_message):
+        self.mp_log.write("{0}::{1}\n".format(endpoint, message))
+
+logging_mp = Mixpanel('3c2e69cea214b3ae6a8ffacf1106f6c8', LoggingConsumer())
 
 mp = Mixpanel('3c2e69cea214b3ae6a8ffacf1106f6c8')
 
@@ -11,9 +19,11 @@ mp.track('gyunseul9', 'Plan Upgraded', {
 	'New Plan': 'Premium'
 })
 
+
 app = Flask(__name__)
 
 @app.route('/')
+
 def main():
 	return render_template('index.html')
 
