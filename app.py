970945bb5e6af6f4ceb033c308e4e0b584511989
김재균 <lonbekim@gmail.com>
2018-02-11 13:47:51 +0900

    delete mixpanel

--- app.py
+++ app.py
@@ -1,24 +1,4 @@
 from flask import Flask, render_template
-from mixpanel import Mixpanel
-
-class LoggingConsumer(object):
-    def __init__(self):
-        self.mp_log = open("MIXPANEL_LOG.txt", "w+")
-
-    def send(self, endpoint, json_message):
-        self.mp_log.write("{0}::{1}\n".format(endpoint, message))
-
-logging_mp = Mixpanel('3c2e69cea214b3ae6a8ffacf1106f6c8', LoggingConsumer())
-
-mp = Mixpanel('3c2e69cea214b3ae6a8ffacf1106f6c8')
-
-
-mp.track('gyunseul9', 'Sent Message')
-mp.track('gyunseul9', 'Plan Upgraded', {
-	'Old Plan': 'Business',
-	'New Plan': 'Premium'
-})
-
 
 app = Flask(__name__)
 
